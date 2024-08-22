33499c7ccc3768c58e684071fcff6191ef1c2417
Fariha Hoque Mimma <49908132+farihahoque@users.noreply.github.com>
2024-08-22 13:33:34 +0600

    Update HomePage.java

--- Automation Testing/Pages/HomePage.java
+++ Automation Testing/Pages/HomePage.java
@@ -11,7 +11,7 @@ public class HomePage extends BasePage{
     public By WriterSelect=By.xpath("//span[contains(text(),'লেখক')]");
     public By AuthorSelect=By.xpath("//h3[normalize-space()='Kazi Nurul Haque']");
     public By ClickBook= new By.ByCssSelector("body > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1) > span:nth-child(1)");
-    public By AnyBook=By.xpath("//a[contains(text(),'আমার নবি মুহাম্মাদ (স)')]");
+    public By AnyBook=By.xpath("//a[contains(text(),'নবি ও ওলিদের বিয়ের গল্প')]");
     public By ReadBook=By.xpath("//button[contains(text(),'একটু পড়ে দেখুন')]");
 
 
